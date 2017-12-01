c370aa097ca140747f826d37d0693191422c3bed
ZhaoLinlin <linlin.zhao@zenjoy.net>
2017-12-01 12:04:10 +0800

    [ZLL] ObservableJust.

--- app/src/main/java/com/mx/rxjavatoy/UI/MainActivity.java
+++ app/src/main/java/com/mx/rxjavatoy/UI/MainActivity.java
@@ -2,7 +2,11 @@ package com.mx.rxjavatoy.UI;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.mx.rxjavatoy.Disposable;
+import com.mx.rxjavatoy.Observable;
+import com.mx.rxjavatoy.Observer;
 import com.mx.rxjavatoy.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +15,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        just();
+    }
+
+    private void just() {
+        Observable<String> observable = Observable.just("hello");
+        observable.subscribe(new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.e("test", "test: onSubscribe");
+            }
+
+            @Override
+            public void onNext(String s) {
+                Log.e("test", "test: onNext " + s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e("test", "test: onError");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.e("test", "test: onComplete");
+            }
+        });
     }
 }
--- app/src/main/java/com/mx/rxjavatoy/imp/ObservableJust.java
+++ app/src/main/java/com/mx/rxjavatoy/imp/ObservableJust.java
@@ -1,5 +1,6 @@
 package com.mx.rxjavatoy.imp;
 
+import com.mx.rxjavatoy.Disposable;
 import com.mx.rxjavatoy.Observable;
 import com.mx.rxjavatoy.Observer;
 
@@ -14,5 +15,30 @@ public class ObservableJust<T> extends Observable<T> {
     @Override
     protected void subscribeActual(Observer<? super T> observer) {
 
+        SimpleDisposable disposable = new SimpleDisposable();
+        observer.onSubscribe(disposable);
+
+        if (!disposable.isDisposed()) {
+            observer.onNext(value);
+        }
+
+        if (!disposable.isDisposed()) {
+            observer.onComplete();
+        }
+    }
+
+    private class SimpleDisposable implements Disposable {
+
+        private boolean disposed;
+
+        @Override
+        public void dispose() {
+            disposed = true;
+        }
+
+        @Override
+        public boolean isDisposed() {
+            return disposed;
+        }
     }
 }
